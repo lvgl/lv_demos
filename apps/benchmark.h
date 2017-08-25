166cd5f2013afb2446d9dbfaae0983f73c61d6b0
Gabor <kisvegabor@gmail.com>
2017-08-25 14:02:35 +0200

    move new apps: benchmark and files

---
+++ apps/benchmark.h
@@ -0,0 +1,37 @@
+/**
+ * @file benchmark.h
+ *
+ */
+
+#ifndef BENCHMARK_H
+#define BENCHMARK_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+/**********************
+ *      MACROS
+ **********************/
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /* BENCHMARK_H */
---
+++ apps/files.h
@@ -0,0 +1,38 @@
+/**
+ * @file files.h
+ *
+ */
+
+#ifndef FILES_H
+#define FILES_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+#include "lvgl/lvgl.h"
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+
+/**********************
+ *      MACROS
+ **********************/
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /*FILES_H*/
