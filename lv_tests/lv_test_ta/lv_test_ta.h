994bf23b43141ca6176906de9827dc2335ab749b
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2017-11-05 14:13:18 +0100

    ta test added

---
+++ lv_tests/lv_test_ta/lv_test_ta.h
@@ -0,0 +1,42 @@
+/**
+ * @file lv_test_ta.h
+ *
+ */
+
+#ifndef LV_TEST_TA_H
+#define LV_TEST_TA_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+#include "lvgl/lvgl.h"
+
+#if USE_LV_TA != 0
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+
+/**********************
+ *      MACROS
+**********************/
+
+#endif /*USE_LV_TA*/
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /*LV_TEST_TA_H*/
