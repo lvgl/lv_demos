e8578bfb3ecf56938afa062a13e0faddd8ac635e
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2018-02-15 13:24:14 +0100

    add lv_test_task

---
+++ lv_tests/lv_test_misc/lv_test_task.h
@@ -0,0 +1,43 @@
+/**
+ * @file lv_test_task.h
+ *
+ */
+
+#ifndef LV_TEST_TASK_H
+#define LV_TEST_TASK_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+#include "../../../lv_ex_conf.h"
+#if USE_LV_TESTS
+
+#include "../../../lvgl/lvgl.h"
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+
+/**********************
+ *      MACROS
+ **********************/
+
+#endif
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /*LV_TEST_TASK_H*/
