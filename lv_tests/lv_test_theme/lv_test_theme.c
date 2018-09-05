1cfcd843525f31de4573442e15a39f7f39d3b3d5
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2018-09-06 01:24:25 +0200

    lv_theme: add live update feature

--- lv_tests/lv_test_theme/lv_test_theme.c
+++ lv_tests/lv_test_theme/lv_test_theme.c
@@ -43,6 +43,7 @@ static void create_tab3(lv_theme_t * th, lv_obj_t * parent);
 void lv_test_theme_1(lv_theme_t * th)
 {
     lv_theme_set_current(th);
+    th = lv_theme_get_current();
     lv_obj_t * scr = lv_cont_create(NULL, NULL);
     lv_scr_load(scr);
     lv_cont_set_style(scr, th->bg);
