a96c6b5e3a0f40f41b4b6fd4497c7bdcdec7ae6a
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2019-01-15 18:59:32 +0100

    add canvas test

---
+++ lv_tests/lv_test_objx/lv_test_canvas/lv_test_canvas.c
@@ -0,0 +1,71 @@
+/**
+ * @file lv_test_cb.c
+ *
+ */
+
+/*********************
+ *      INCLUDES
+ *********************/
+#include "lv_test_canvas.h"
+
+#if USE_LV_CANVAS && USE_LV_TESTS
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ *  STATIC PROTOTYPES
+ **********************/
+
+/**********************
+ *  STATIC VARIABLES
+ **********************/
+
+/**********************
+ *      MACROS
+ **********************/
+
+/**********************
+ *   GLOBAL FUNCTIONS
+ **********************/
+
+/**
+ * Create canvas to test its functionalities
+ */
+void lv_test_canvas_1(void)
+{
+    /*Create a buffer for the canvas set its pixels to gray*/
+    static lv_color_t cbuf[sizeof(lv_color_t) * 100 * 80];
+    uint32_t i;
+    for(i = 0; i < sizeof(cbuf) / sizeof(cbuf[0]); i++) {
+        cbuf[i] = LV_COLOR_GRAY;
+    }
+
+    /*Create the canvas object*/
+    lv_obj_t * canvas = lv_canvas_create(lv_scr_act(), NULL);
+
+    /*Assign the buffer to the canvas*/
+    lv_canvas_set_buffer(canvas, cbuf, 100, 80, LV_IMG_CF_TRUE_COLOR);
+
+    /*Set some pixels*/
+    lv_canvas_set_px(canvas, 1,1, LV_COLOR_RED);
+    lv_canvas_set_px(canvas, 2,5, LV_COLOR_BLUE);
+
+    /*Create a pattern to copy to the canvas*/
+    lv_color_t pbuf[2][2] = {LV_COLOR_RED, LV_COLOR_GREEN, LV_COLOR_BLUE, LV_COLOR_AQUA};
+
+    /*Copy the pattern to teh canvas*/
+    lv_canvas_copy_to_buf(canvas, pbuf, 2, 2, 6, 10);
+}
+
+
+/**********************
+ *   STATIC FUNCTIONS
+ **********************/
+
+#endif /*USE_LV_CANVAS && USE_LV_TESTS*/
---
+++ lv_tests/lv_test_objx/lv_test_canvas/lv_test_canvas.h
@@ -0,0 +1,53 @@
+/**
+ * @file lv_test_cb.h
+ *
+ */
+
+#ifndef LV_TEST_CB_H
+#define LV_TEST_CB_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+#ifdef LV_CONF_INCLUDE_SIMPLE
+#include "lvgl.h"
+#include "lv_ex_conf.h"
+#else
+#include "../../../../lvgl/lvgl.h"
+#include "../../../../lv_ex_conf.h"
+#endif
+
+#if USE_LV_CANVAS && USE_LV_TESTS
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+
+/**
+ * Create canvas to test its functionalities
+ */
+void lv_test_canvas_1(void);
+
+/**********************
+ *      MACROS
+ **********************/
+
+#endif /*USE_LV_CANVAS && USE_LV_TESTS*/
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /*LV_TEST_CB_H*/
