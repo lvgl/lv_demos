982db8c797bf135fff96c38c51609282ea117505
seyyah <nurettin.senyer@gmail.com>
2019-01-25 14:31:14 +0300

    lv_canvas: add basic drawing functionalities

--- lv_tests/lv_test_objx/lv_test_canvas/lv_test_canvas.c
+++ lv_tests/lv_test_objx/lv_test_canvas/lv_test_canvas.c
@@ -105,6 +105,99 @@ void lv_test_canvas_1(void)
     }
 }
 
+void lv_test_canvas_2(void)
+{
+    lv_color_t bgcolor = LV_COLOR_MAKE(0xec, 0xf0, 0xf1);
+
+    /*Create a buffer for the canvas set its pixels to light blue*/
+    static lv_color_t cbuf[sizeof(lv_color_t) * 100 * 80];
+    uint32_t i;
+    for(i = 0; i < sizeof(cbuf) / sizeof(cbuf[0]); i++) {
+        cbuf[i] = bgcolor;
+    }
+
+    /*Create the canvas object*/
+    lv_obj_t * canvas = lv_canvas_create(lv_scr_act(), NULL);
+
+    /*Assign the buffer to the canvas*/
+    lv_canvas_set_buffer(canvas, cbuf, 100, 80, LV_IMG_CF_TRUE_COLOR_ALPHA);
+
+    // a. Draw image
+    LV_IMG_DECLARE(img_flower_icon);
+    lv_canvas_copy_buf(canvas, img_flower_icon.data, img_flower_icon.header.w, img_flower_icon.header.h, 0, 0);
+
+    // b. Draw complex objects
+    lv_point_t points5[] = {
+      {.x=10, .y=10},
+      {.x=40, .y=10},
+      {.x=30, .y=40},
+      {.x=20, .y=40},
+    };
+
+    lv_canvas_draw_polygon(canvas, points5, 4, LV_COLOR_RED);
+    lv_canvas_fill_polygon(canvas, points5, 4, LV_COLOR_RED, bgcolor);
+
+    lv_canvas_draw_circle(canvas, 25, 54, 10, LV_COLOR_BLACK);
+    lv_canvas_flood_fill(canvas, 25, 54, bgcolor, LV_COLOR_RED);
+
+    lv_canvas_draw_circle(canvas, 25, 54, 5, LV_COLOR_BLACK);
+    lv_canvas_draw_circle(canvas, 25, 54, 15, LV_COLOR_BLACK);
+
+    // c. Draw polygon/fill
+    lv_point_t points4[] = {
+      {.x=10, .y=10},
+      {.x=40, .y=10},
+      {.x=30, .y=40},
+      {.x=20, .y=40},
+    };
+
+    lv_canvas_draw_polygon(canvas, points4, 4, LV_COLOR_RED);
+    lv_canvas_fill_polygon(canvas, points4, 4, LV_COLOR_RED, bgcolor);
+
+    // d. Draw rect/fill
+    lv_point_t points3[4] = {
+      {.x=10, .y=10},
+      {.x=50, .y=10},
+      {.x=50, .y=50},
+      {.x=10, .y=50}
+    };
+
+    lv_canvas_draw_rect(canvas, points3, LV_COLOR_RED);
+    lv_canvas_fill_polygon(canvas, points3, 4, LV_COLOR_RED, bgcolor);
+
+    // e. Draw triangle/fill
+    lv_point_t points1[3] = {
+      {.x=50, .y=10},
+      {.x=30, .y=50},
+      {.x=70, .y=50}
+    };
+    lv_canvas_draw_triangle(canvas, points1, LV_COLOR_RED);
+    lv_canvas_fill_polygon(canvas, points1, 3, LV_COLOR_RED, bgcolor);
+
+    lv_point_t points2[3] = {
+      {.x=80, .y=5},
+      {.x=50, .y=30},
+      {.x=99, .y=79}
+    };
+    lv_canvas_draw_triangle(canvas, points2, LV_COLOR_BLUE);
+    lv_canvas_fill_polygon(canvas, points2, 3, LV_COLOR_BLUE, bgcolor);
+
+    // f. Draw lines
+    lv_point_t point1 = {.x=50, .y=10}, point2 = {.x=30, .y=50}, point3 = {.x=70, .y=50};
+    lv_canvas_draw_line(canvas, point1, point2, LV_COLOR_RED);
+    lv_canvas_draw_line(canvas, point1, point3, LV_COLOR_RED);
+    lv_canvas_draw_line(canvas, point3, point2, LV_COLOR_RED);
+    lv_canvas_flood_fill(canvas, 50, 30, LV_COLOR_RED, bgcolor);
+
+    // h. Draw circle
+    lv_canvas_draw_circle(canvas, 50, 40, 15, LV_COLOR_BLACK);
+    lv_canvas_flood_fill(canvas, 50, 40, LV_COLOR_RED, bgcolor);
+
+    lv_canvas_draw_circle(canvas, 50, 40,  5, LV_COLOR_BLACK);
+    lv_canvas_draw_circle(canvas, 50, 40, 10, LV_COLOR_BLACK);
+    lv_canvas_draw_circle(canvas, 50, 40, 20, LV_COLOR_BLACK);
+    lv_canvas_draw_circle(canvas, 50, 40, 25, LV_COLOR_BLACK);
+}
 
 /**********************
  *   STATIC FUNCTIONS
