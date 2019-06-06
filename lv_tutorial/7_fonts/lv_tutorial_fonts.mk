61d996d30340b588b39c96e78867022accdd5a2f
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2019-06-06 05:59:51 +0200

    fix makefile

--- lv_tutorial/7_fonts/lv_tutorial_fonts.mk
+++ lv_tutorial/7_fonts/lv_tutorial_fonts.mk
@@ -1,7 +1,5 @@
 CSRCS += lv_tutorial_fonts.c
-CSRCS += arial_ascii_20.c
-CSRCS += arial_cyrillic_20.c
-CSRCS += arial_math_20.c
+CSRCS += arial_20.c
 
 DEPPATH += --dep-path $(LVGL_DIR)/lv_examples/lv_tutorial/7_fonts
 VPATH += :$(LVGL_DIR)/lv_examples/lv_tutorial/7_fonts
