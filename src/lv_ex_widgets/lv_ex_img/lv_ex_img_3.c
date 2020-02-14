1a52f532708573132474955a2a816569797912c0
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2020-02-14 12:22:26 +0100

    add objmask and img transform examples

--- src/lv_ex_widgets/lv_ex_img/lv_ex_img_3.c
+++ src/lv_ex_widgets/lv_ex_img/lv_ex_img_3.c
@@ -8,10 +8,8 @@ void lv_ex_img_3(void)
     /* Now create the actual image */
     lv_obj_t * img = lv_img_create(lv_scr_act(), NULL);
     lv_img_set_src(img, &cogwheel);
-    lv_obj_align(img, NULL, LV_ALIGN_CENTER, 0, 0);
-    lv_img_set_pivot(img, 0, 50);
-    lv_img_set_zoom(img, 512);
-    lv_img_set_angle(img, 90);
+    lv_obj_align(img, NULL, LV_ALIGN_CENTER, 50, 50);
+    lv_img_set_pivot(img, 0, 0);    /*Rotate around the top left corner*/
 
     lv_anim_t a;
     lv_anim_init(&a);
@@ -21,10 +19,10 @@ void lv_ex_img_3(void)
     lv_anim_set_time(&a, 5000, 0);
     lv_anim_create(&a);
 
-//    lv_anim_set_exec_cb(&a, img, (lv_anim_exec_xcb_t)lv_img_set_zoom);
-//    lv_anim_set_values(&a, 128, 512);
-//    lv_anim_set_playback(&a, 0);
-//    lv_anim_create(&a);
+    lv_anim_set_exec_cb(&a, img, (lv_anim_exec_xcb_t)lv_img_set_zoom);
+    lv_anim_set_values(&a, 128, 256);
+    lv_anim_set_playback(&a, 0);
+    lv_anim_create(&a);
 
 }
 
---
+++ src/lv_ex_widgets/lv_ex_objmask/index.rst
@@ -0,0 +1,36 @@
+C
+^
+
+Several object masks
+"""""""""""""""""""
+
+.. image:: /lv_examples/src/lv_ex_page/lv_ex_objmask_1.*
+  :alt: Page example in LittlevGL
+
+.. container:: toggle
+
+    .. container:: header
+    
+      code
+
+    .. literalinclude:: /lv_examples/src/lv_ex_page/lv_ex_objmask_1.c
+      :language: c
+      
+      
+Text mask
+"""""""""""""""""""
+
+.. image:: /lv_examples/src/lv_ex_page/lv_ex_objmask_2.*
+  :alt: Page example in LittlevGL
+
+.. container:: toggle
+
+    .. container:: header
+    
+      code
+
+    .. literalinclude:: /lv_examples/src/lv_ex_page/lv_ex_objmask_2.c
+      :language: c
+
+MicroPython
+^^^^^^^^^^^
