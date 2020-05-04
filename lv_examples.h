5fd053b8d87fa988526d255cb4b42e06afc8b235
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2020-05-04 09:54:44 +0200

    Fix including of lv_examples.h

--- lv_examples.h
+++ lv_examples.h
@@ -13,7 +13,7 @@ extern "C" {
 /*********************
  *      INCLUDES
  *********************/
-#if defined(LV_CONF_INCLUDE_SIMPLE)
+#if defined(LV_LVGL_H_INCLUDE_SIMPLE)
 #include "lvgl.h"
 #else
 #include "../lvgl/lvgl.h"
