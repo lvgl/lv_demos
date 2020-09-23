4a6def8b350166fec430e8409c0c2fe8fd516d07
Themba Dube <embeddedthemba@gmail.com>
2020-09-23 14:42:47 -0400

    Automatically build Emscripten versions of examples

---
+++ scripts/build_all_examples.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+GIT_HASH=$1
+
+if [ "x$GIT_HASH" == "" ]; then
+GIT_HASH=$(git rev-parse HEAD)
+fi
+
+mkdir -p objs
+cd objs
+echo "-- Updating builder ..."
+OBJS_DIR=`pwd`
+if ! [ -d builder ]; then
+  git clone --quiet --recursive https://github.com/lvgl/lv_sim_emscripten.git builder
+  cd builder
+else
+  cd builder
+  git pull --quiet
+fi
+cd lv_examples
+echo "-- Checking out lv_examples $GIT_HASH ..."
+git checkout $GIT_HASH
+cd ..
+echo "<html><body><h1>Choose an example:</h1><ul>" > $OBJS_DIR/index.html
+cat $OBJS_DIR/../scripts/examples.txt | while read -r example_name; do
+  echo "-- Building $example_name ..."
+  make -j4 CHOSEN_DEMO=$example_name NO_GIT_HASH=1 || exit 1
+  rm -rf $OBJS_DIR/$example_name
+  cp -a build $OBJS_DIR/$example_name
+  echo "<li><a href=\"$example_name/index.html\">$example_name</a></li>" >> $OBJS_DIR/index.html
+done
+echo "</ul></body></html>" >> $OBJS_DIR/index.html
