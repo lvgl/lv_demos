a4998db8a4c172e2127edce1b53f9f6e37692407
Uli Raich <uli.raich@gmail.com>
2020-11-24 16:04:36 +0100

    add ex9 of style

---
+++ src/lv_ex_widgets/lv_ex_img/ex1_img_sim.py
@@ -0,0 +1,23 @@
+with open('../../../assets/img_cogwheel_argb.bin','rb') as f:
+    cogwheel_img_data = f.read()
+    
+# create the cogwheel image data
+
+cogwheel_img_dsc = lv.img_dsc_t(
+    {
+        "header": {"always_zero": 0, "w": 100, "h": 100, "cf": lv.img.CF.TRUE_COLOR_ALPHA},
+        "data": cogwheel_img_data,
+        "data_size": len(cogwheel_img_data),
+    }
+)
+# Create an image using the decoder
+
+img1 = lv.img(lv.scr_act(),None)
+lv.img.cache_set_size(2)
+img1.align(lv.scr_act(), lv.ALIGN.CENTER, 0, -50)
+img1.set_src(cogwheel_img_dsc)
+
+img2 = lv.img(lv.scr_act(), None)
+img2.set_src(lv.SYMBOL.OK+"Accept")
+img2.align(img1, lv.ALIGN.OUT_BOTTOM_MID, 0, 20)
+
