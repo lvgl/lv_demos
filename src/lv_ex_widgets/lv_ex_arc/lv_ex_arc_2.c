37c79597b24b39e979c0ad06ac75b3cbddfaa5e8
Themba Dube <embeddedthemba@gmail.com>
2020-11-27 21:18:26 -0500

    Follow dev

--- src/lv_ex_widgets/lv_ex_arc/lv_ex_arc_2.c
+++ src/lv_ex_widgets/lv_ex_arc/lv_ex_arc_2.c
@@ -5,7 +5,7 @@
  * An `lv_task` to call periodically to set the angles of the arc
  * @param t
  */
-static void arc_loader(lv_tmr_t * t)
+static void arc_loader(lv_timer_t * t)
 {
     static int16_t a = 270;
 
@@ -14,7 +14,7 @@ static void arc_loader(lv_tmr_t * t)
     lv_arc_set_end_angle(t->user_data, a);
 
     if(a >= 270 + 360) {
-        lv_tmr_del(t);
+        lv_timer_del(t);
         return;
     }
 }
@@ -32,7 +32,7 @@ void lv_ex_arc_2(void)
 
   /* Create an `lv_task` to update the arc.
    * Store the `arc` in the user data*/
-  lv_tmr_create(arc_loader, 20, arc);
+  lv_timer_create(arc_loader, 20, arc);
 }
 
 #endif
