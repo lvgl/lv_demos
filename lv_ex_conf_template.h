c4c363612aba4b3d6b6b623b7b7c5ca08d9e2fc3
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2020-12-02 14:13:37 +0100

    update lv_ex_conf_template.h

--- lv_ex_conf_template.h
+++ lv_ex_conf_template.h
@@ -42,6 +42,12 @@
 /*Stress test for LVGL*/
 #define LV_USE_DEMO_STRESS      0
 
+/*Music player for LVGL*/
+#define LV_USE_DEMO_MUSIC      0
+#if LV_USE_DEMO_MUSIC
+#define LV_DEMO_MUSIC_AUTO_PLAY 0
+#endif
+
 #endif /*LV_EX_CONF_H*/
 
 #endif /*End of "Content enable"*/
