6b9e5ee5dc3173531fa5bb89c7f9f7145d49542b
Themba Dube <embeddedthemba@gmail.com>
2020-12-03 11:01:03 -0500

    Add lv_demo_music to lv_demo_music.h

--- src/lv_demo_music/lv_demo_music.h
+++ src/lv_demo_music/lv_demo_music.h
@@ -31,6 +31,7 @@ extern "C" {
  * GLOBAL PROTOTYPES
  **********************/
 
+void lv_demo_music(void);
 const char * _lv_demo_music_get_title(uint32_t track_id);
 const char * _lv_demo_music_get_artist(uint32_t track_id);
 const char * _lv_demo_music_get_genre(uint32_t track_id);
